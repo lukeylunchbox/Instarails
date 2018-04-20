1c80b91949773c84133737ee0aea321bd2875f5f
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-04-20 12:37:16 +1000

    Add routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  root 'pages#home'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
