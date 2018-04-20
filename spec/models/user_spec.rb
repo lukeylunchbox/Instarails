19aa5cea596a09230401e52883b9d73f42b10fb7
Luke Colcott <git@github.com:lukeylunchbox/test.git>
2018-04-20 12:25:30 +1000

    first commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'should have matching email' do
+    user = User.new
+    user.email = ENV.fetch('USER_EMAIL')
+    expect(user.email).to eq("l.colcott@live.com")
+  end 
+end
